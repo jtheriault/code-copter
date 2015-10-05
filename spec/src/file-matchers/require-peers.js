59a246b089665b8f419c3123273cf4c8b0a323c9
Joe Theriault <joseph@yankoismedia.com>
2015-10-04 23:14:23 -0400

    Style conformance

--- spec/src/file-matchers/require-peers.js
+++ spec/src/file-matchers/require-peers.js
@@ -6,14 +6,12 @@ module.exports = toOnlyRequirePeers;
  */
 function toOnlyRequirePeers () {
     return {
-        compare: function compare (actual, expected) {
+        compare: function compare (actual) {
             var result = { pass: true },
                 errors;
 
-            errors = 
-                // All local file requires
-                (actual.match(/require\((['"])\..*?\1\)/g) || [])
-                // Catch any up/down-directory requires
+            // TODO: Capture line number
+            errors = (actual.match(/require\((['"])\..*?\1\)/g) || [])
                 .filter(fileRequire => fileRequire.match(/\./g).length > 1 || fileRequire.match(/\//g).length > 1)
                 .map(statement => `Requiring non-peer, non-package module: ${statement}`);
                 
@@ -24,7 +22,6 @@ function toOnlyRequirePeers () {
             }
             else {
                 result.message = errors
-                //.map(error => `line ${error.line}:\t${error.message}`)
                     .join('\n');
             }
 
